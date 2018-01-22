736e430f9959f4cef03aacf47638891572203e5f
gjhughes <ghughes@me.com>
2018-01-22 23:51:35 +0000

    heroku push

--- wdi-project-2/config/environment.js
+++ wdi-project-2/config/environment.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3000,
+  port: 3000,
   dbUri: process.env.MONGODB_URI || 'mongodb://localhost/find-craftdb',
   sessionSecret: process.env.SESSION_SECRET || 'YghT5s617/1{%sDt'
 };
