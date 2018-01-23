9c591fe54659561d096c4c75ecec76110298769e
gjhughes <ghughes@me.com>
2018-01-23 01:04:49 +0000

    env port config

--- wdi-project-2/config/environment.js
+++ wdi-project-2/config/environment.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: process.env.PORT || 3000,
   dbUri: process.env.MONGODB_URI || 'mongodb://localhost/find-craftdb',
-  sessionSecret: process.env.SESSION_SECRET || 'YghT5s617/1{%sDt'
+  sessionSecret: process.env.SESSION_SECRET || 'YghT5s617/1{%sDt',
 };
